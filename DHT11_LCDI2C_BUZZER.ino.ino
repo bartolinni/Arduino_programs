5a92738b19bc872e17a919db46c9d71dcc28f6ce
bartolinni <84548043+bartolinni@users.noreply.github.com>
2021-07-18 15:08:32 -0300

    Add files via upload
    Programa para monitoramento instantâneo de temperatura e umidade do ar. Um buzzer conectado ao pino 6 soa quando a temperatura t atinge um valor pré-determinado no programa

---
+++ DHT11_LCDI2C_BUZZER.ino.ino
@@ -0,0 +1,96 @@
+
+// Código criado para avisar através de um bip temperaturas do ar acima de 27 °C
+// Componentes: Arduino Uno, sensor DHT 11, display LCD 16x2 acoplado com um módulo I2C no endereço 0x27, Buzzer
+// Criado inicialmente para apitar quando a temperatura de uma caixa aquecida de kefir de água atingir valores de 27°C ou mais
+// Implementações futuras: conectar um relé para controlar o acender e apagar do aquecedor (lâmpada incandescente)
+
+
+#include "DHT.h"
+#define DHTPIN 2     // Pino digital sensor DHT
+
+// Escolha o tipo de sensor!
+# define DHTTYPE DHT11     // DHT 11
+//# define DHTTYPE DHT22   // DHT 22 (AM2302), AM2321
+//# define DHTTYPE DHT21   // DHT 21 (AM2301)
+
+DHT dht(DHTPIN, DHTTYPE);
+
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+// Definir o endereço do LCD para 0x27 para um display de 16 caracteres e 2 linhas
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println(F("DHTxx teste!"));
+
+  dht.begin();
+
+  lcd.begin();
+
+  pinMode(6, OUTPUT);
+  analogWrite(6, 0);
+}
+
+void loop() {
+  // Aguarde alguns segundos entre as medições.
+
+  // delay(3000);
+
+  // A leitura da temperatura ou umidade leva cerca de 250 milissegundos!
+  // O sensor pode ter um atraso de até 2 segundos para a leitura
+  
+  float h = dht.readHumidity();
+  // Temperature em Celsius (default)
+  float t = dht.readTemperature();
+  // Temperature em Fahrenheit (Fahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Verifique se alguma leitura falhou e tenta novamente.
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.print(F("Falha de leitura do sensor DHT!"));
+    return;
+  }
+
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  lcd.setBacklight(HIGH);
+
+  lcd.setCursor(0, 0);
+  lcd.print(F("T = "));
+  lcd.setCursor(4, 0);
+  lcd.print(round(t));
+  lcd.setCursor(7, 0);
+  lcd.write(223);
+  lcd.setCursor(8, 0);
+  lcd.print("C");
+
+  lcd.setCursor(0, 1);
+  lcd.print(F("H = "));
+  lcd.setCursor(4, 1);
+  lcd.print(round(h));
+  lcd.setCursor(7, 1);
+  lcd.print("%");
+
+  if (t >= 27) {
+    analogWrite(6, 3);
+    delay(100);
+    analogWrite(6, 0);
+    delay(100);
+    analogWrite(6, 3);
+    delay(100);
+    analogWrite(6, 0);
+    delay(100);
+    analogWrite(6, 3);
+    delay(100);
+    analogWrite(6, 0);
+    
+  }
+
+  delay(2000);
+  
+}
